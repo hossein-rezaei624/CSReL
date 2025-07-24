8a0edf5db8c5f087d719e44692cb0954a0dfb4bf
Hossein Rezaei <hr505@cl.cam.ac.uk>
2025-07-24 18:19:55 +0330

    Update run_stream_tiny_500.sh

--- scripts/run_stream_tiny_500.sh
+++ scripts/run_stream_tiny_500.sh
@@ -1,6 +1,6 @@
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-local_path='./results/stream_tiny_imagenet/test2'  # set your output path
+local_path='./results/stream_tiny_imagenet/test1'  # set your output path
 dataset='splittinyimagenet'
 data_path='./tiny_imagenet_from_DER'  # need to download tiny-imagenet dataset
 buffer_size=500

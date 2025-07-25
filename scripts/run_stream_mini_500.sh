d30be2e527b13bd5fd8e0e9723fc94c6f94ae917
Hossein Rezaei <hr505@cl.cam.ac.uk>
2025-07-25 07:03:28 +0330

    Update run_stream_mini_500.sh

--- scripts/run_stream_mini_500.sh
+++ scripts/run_stream_mini_500.sh
@@ -1,6 +1,6 @@
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-local_path='./results/stream_mini_imagenet/test1'  # set your output path
+local_path='./results/stream_mini_imagenet/test2'  # set your output path
 dataset='splitminiimagenet'
 data_path='./mini_imagenet_from_DER'  # need to download tiny-imagenet dataset
 buffer_size=500
@@ -20,7 +20,7 @@ cur_train_lr=5e-3
 cur_train_steps=20
 selection_steps=250
 setting=''
-seed=0
+seed=1
 
 
 python3 -u stream_continual_learning.py --local_path=$local_path \

44636066f48d1601e8d22d8bff9f2f4fc6355ed5
Hossein Rezaei <hr505@cl.cam.ac.uk>
2025-07-26 09:37:57 +0330

    Update continual_runner.py

--- continual_learning/continual_runner.py
+++ continual_learning/continual_runner.py
@@ -117,7 +117,7 @@ class ContinualRunner(object):
         for i in range(self.train_params['epochs']):
             step = 0
             for data in train_loader:
-                ___, aug_sp, lab = data
+                aug_sp, lab = data
                 if self.use_cuda:
                     aug_sp = aug_sp.cuda()
                     lab = lab.cuda()
@@ -240,10 +240,10 @@ class ContinualRunner(object):
 
     def update_buffer(self, full_train_loader, sub_loader, next_loader=None):
         # make current x and y
-        ___, x, y = next(iter(full_train_loader))
+        x, y = next(iter(full_train_loader))
         full_cur_x = x.numpy()
         full_cur_y = y.numpy()
-        ___, cur_x, cur_y = next(iter(sub_loader))
+        cur_x, cur_y = next(iter(sub_loader))
         self.task_cnts.append(cur_x.shape[0])
         if 'beta' in self.train_params:
             temp_data = []

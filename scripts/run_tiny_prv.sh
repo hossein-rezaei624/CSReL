4f79ef3eb9408aa7c71787eb3c2172503a953643
Hossein Rezaei <hr505@cl.cam.ac.uk>
2025-07-28 13:50:26 +0330

    Update run_tiny_prv.sh

--- scripts/run_tiny_prv.sh
+++ scripts/run_tiny_prv.sh
@@ -1,6 +1,6 @@
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-local_path='./results/split_tiny_imagenet/test4'  # set your output path
+local_path='./results/split_tiny_imagenet/test5'  # set your output path
 dataset='splittinyimagenet'
 setting='der'
 data_path='./tiny_imagenet_from_DER'
@@ -23,7 +23,7 @@ runner_type='coreset'
 update_mode='coreset'
 extra_data=''
 ref_train_epoch=20
-selection_steps=100
+selection_steps=250
 cur_train_steps=7
 buffer_type='coreset'
 aug_type='der'

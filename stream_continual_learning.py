88688acbfafe1b812cfe1b1578a7e5712327cbdc
Hossein Rezaei <hr505@cl.cam.ac.uk>
2025-08-04 20:58:56 +0330

    Update stream_continual_learning.py

--- stream_continual_learning.py
+++ stream_continual_learning.py
@@ -116,6 +116,9 @@ def main(opts):
         
         task_class_.update({value: i for index, value in enumerate(unique_classes_)})
         print("task_class_", task_class_)
+        temp_new = torch.stack(runner.buffer.data, dim=0)
+        print("temp_new.shape", temp_new.shape)
+        print("temp_new[2,:]", temp_new[2,:])
         print("runner.buffer.data[0]", runner.buffer.data[0])
         print("len of buffer data", len(runner.buffer.data))
 
@@ -189,6 +192,7 @@ if __name__ == '__main__':
     print('seed\t\t', args.seed)
     print('\n')
     main(opts=args)
+
 
 
 
